fff9822be88f3add1c4e4d4139dc526cb1d11d29
Zhou, Zhiqian <32314427+zzqCindy@users.noreply.github.com>
2018-04-28 18:49:57 -0500

    connect DB

---
+++ includefiles/connect.php
@@ -0,0 +1,26 @@
+<?php
+    $server="tigerbook.czizqzi5ilgp.us-east-1.rds.amazonaws.com:3306";//主机
+    $db_username="admin";//你的数据库用户名
+    $db_password="admin1234";//你的数据库密码
+
+    $con = mysqli_connect($server,$db_username,$db_password);//链接数据库
+    if(!$con){
+        die("can't connect".mysql_error());//如果链接失败输出错误
+    }
+    
+    mysqli_select_db($con,'tigerbook');//选择数据库
+
+    /*
+    $ID = 2;
+    $User = "user2";
+    $Pass = "123";
+    $q="insert into Login values ('$ID','$User','$Pass')";//向数据库插入表单传来的值的sql
+    $reslut=mysqli_query($con,$q);//执行sql
+    if (!$reslut){
+        die('Error: ' . mysqli_error());//如果sql执行失败输出错误
+    }
+     */
+    
+    mysqli_close($con);
+
+?>
